8a0720fa23595ac02d6497b812282416b232aa31
jy00884666 <87419590@qq.com>
2022-08-17 16:58:48 +0800

    20220817

--- src/main/java/sy/annotations/Sync.java
+++ src/main/java/sy/annotations/Sync.java
@@ -15,30 +15,32 @@ import java.lang.annotation.Target;
 
 /**
  * 并发控制的注解
+ * @author shashijie
  * @description:
  * @reason: ADD REASON(可选)
- * @author shashijie
  * @date 2017-01-25
  * @since JDK 1.7
  */
 // Target 设置注解权限,定义此注解可以申明在哪里 METHOD:方法上,TYPE:类上
-@Target({ ElementType.METHOD, ElementType.TYPE })
+@Target({ElementType.METHOD, ElementType.TYPE})
 // Retention生命周期 Runtime:运行时
 @Retention(RetentionPolicy.RUNTIME)
-// 注解自动继承,类和子类都包含这个注解
+/**在定义一个作用于类的注解时候，如果希望该注解也作用于其子类，那么可以用@Inherited 来进行修饰
+ * 结论：被 @Inherited 注解修饰的注解，如果作用于某个类上，其子类是可以继承的该注解的。
+ * 反之，如果一个注解没有被 @Inherited注解所修饰，那么他的作用范围只能是当前类，其子类是不能被继承的*/
 @Inherited
 public @interface Sync {
-
+    
     /**
      * shashijie 2017-01-25 表示:同步锁的KEY值,若没有默认为方法名
      * @return String
      */
     public String name() default "";
-
+    
     /**
      * shashijie 2017-01-25 表示:同步锁的key = 方法名+第几个入参的值,数组形式可以是多个
      * @return int[]
      */
     public int[] paramIndexs() default {};
-
+    
 }
